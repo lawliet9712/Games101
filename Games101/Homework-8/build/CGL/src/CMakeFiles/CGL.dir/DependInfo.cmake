065ea57c620fcdae5e8e2aaae2833718e7e02cdf
lawliet <www.wwj19@qq.com>
2023-09-20 15:03:49 +0800

    finish games101 homework 8

---
+++ Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/osdfont.c" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/osdfont.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "../CGL/deps/glew/include"
+  "/usr/include/freetype2"
+  "../CGL/src"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/base64.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/base64.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/color.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/color.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/complex.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/complex.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/matrix3x3.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/matrix3x3.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/matrix4x4.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/matrix4x4.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/osdtext.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/osdtext.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/quaternion.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/quaternion.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/tinyxml2.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/tinyxml2.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/vector2D.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/vector2D.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/vector3D.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/vector3D.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/vector4D.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/vector4D.cpp.o"
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/CGL/src/viewer.cpp" "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/src/CMakeFiles/CGL.dir/viewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../CGL/include"
+  "../CGL/deps/glew/include"
+  "/usr/include/freetype2"
+  "../CGL/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lawliet/Games101/Games101/Games101/Homework-8/build/CGL/deps/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Games101/Homework-8/src/rope.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+#include "CGL/vector2D.h"
+
+#include "mass.h"
+#include "rope.h"
+#include "spring.h"
+
+namespace CGL {
+
+    Vector2D normalize(Vector2D vec) {
+        double length = std::sqrt(vec.x * vec.x + vec.y * vec.y);
+        vec.x /= length;
+        vec.y /= length;
+        return vec;
+    }
+
+    Rope::Rope(Vector2D start, Vector2D end, int num_nodes, float node_mass, float k, vector<int> pinned_nodes)
+    {
+        // TODO (Part 1): Create a rope starting at `start`, ending at `end`, and containing `num_nodes` nodes.
+        for(int i=0; i<num_nodes; ++i) {
+            Vector2D pos = start + (end - start) * ((double)i / ((double)num_nodes - 1.0));          
+            masses.push_back(new Mass(pos, node_mass, false));
+            // masses[i]->forces = Vector2D(0, 0);
+        }
+
+        for(int i=0; i<num_nodes-1; ++i) {
+            springs.push_back(new Spring(masses[i], masses[i+1], k));
+        }
+
+//        Comment-in this part when you implement the constructor
+       for (auto &i : pinned_nodes) {
+           masses[i]->pinned = true;
+       }
+    }
+
+    void Rope::simulateEuler(float delta_t, Vector2D gravity)
+    {
+        // 
+        for (auto &s : springs)
+        {
+            // TODO (Part 2): Use Hooke's law to calculate the force on a node
+            auto len = (s->m1->position - s->m2->position).norm();
+            s->m1->forces += -s->k * (s->m1->position - s->m2->position) / len * (len - s->rest_length);
+            s->m2->forces += -s->k * (s->m2->position - s->m1->position) / len * (len - s->rest_length); 
+        }
+
+        float k_d = 0.1f;
+        for (auto &m : masses)
+        {
+            if (!m->pinned)
+            {
+                // TODO (Part 2): Add the force due to gravity, then compute the new velocity and position
+                auto a = m->forces / m->mass + gravity;               
+                // m->position += m->velocity * delta_t; // For explicit method
+                m->velocity += a * delta_t; 
+                m->position += m->velocity * delta_t; // For semi-implicit method
+            }
+
+            // Reset all forces on each mass
+            m->forces = Vector2D(0, 0);
+        }
+    }
+
+    void Rope::simulateVerlet(float delta_t, Vector2D gravity)
+    {
+        for (auto &s : springs)
+        {
+            // TODO (Part 3): Simulate one timestep of the rope using explicit Verlet （solving constraints)
+            auto len = (s->m1->position - s->m2->position).norm();
+            s->m1->forces += -s->k * (s->m1->position - s->m2->position) / len * (len - s->rest_length);
+            s->m2->forces += -s->k * (s->m2->position - s->m1->position) / len * (len - s->rest_length);
+        }
+
+        float damping_factor = 0.00005;
+        for (auto &m : masses)
+        {
+            if (!m->pinned)
+            {
+                Vector2D temp_position = m->position;
+                auto a = m->forces / m->mass + gravity;
+                // TODO (Part 3.1): Set the new position of the rope mass
+                m->position = temp_position + (1 - damping_factor) * (temp_position - m->last_position) + a * delta_t * delta_t;
+                m->last_position = temp_position;
+            }
+            m->forces = Vector2D(0, 0);
+        }
+    
+    }
+}
